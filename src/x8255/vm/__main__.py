ee1de65d31efaf8d7cf4a792ee6549a87aa86fa3
iiPython <ben@iipython.dev>
2026-06-28 17:11:31 -0400

    add --speed argument to emulator

--- src/x8255/vm/__main__.py
+++ src/x8255/vm/__main__.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2026 iiPython
 
+from time import sleep
 from pathlib import Path
 
 from x8255.cli import p, cexit
@@ -8,7 +9,8 @@ from x8255.vm.core import Emu8255
 
 def main() -> None:
     p.add_argument("-N", "--no-drivers", action = "store_true", help = "disable all drivers", default = False)
-    p.add_argument("-d", "--drivers", help = "enabled list of drivers, default is stdio", default = "stdio")
+    p.add_argument("-d", "--drivers", help = "enabled list of drivers, default: stdio", default = "stdio")
+    p.add_argument("-s", "--speed", type = int, help = "emulation speed in hertz, default: no limit", default = 0)
     p.add_argument("executable", type = Path, help = "path to compiled executable")
 
     args = p.parse_args()
@@ -20,11 +22,18 @@ def main() -> None:
 
     bytecode = file.read_bytes()
 
+    # Confirm speed
+    speed = args.speed
+    if speed < 0:
+        cexit("Provided speed argument is negative and thus invalid.")
+
     # Setup emulation
     system = Emu8255(args.drivers.split(",") if not args.no_drivers else [])
     system.write_range(bytecode[:Addresses.CODE.size], Addresses.CODE.start)
     system.write_range(bytecode[Addresses.CODE.size:], Addresses.CODE.end)
 
     # System loop
+    delay = 1 / speed if speed != 0 else 0
     while True:
         system.step()
+        sleep(delay)
