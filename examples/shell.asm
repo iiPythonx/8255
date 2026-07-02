99bd37f52651515bf8944e82dfe2d93e2b8cfcd9
iiPython <ben@iipython.dev>
2026-07-02 13:08:53 -0400

    nocturne: add mem command

--- examples/shell.asm
+++ examples/shell.asm
@@ -4,18 +4,22 @@ preload:
     .clear     "\033[2J\033[H"
     .return    "\033[2K\r"
     .prompt    "\033[32m$\033[0m "
-    .execute   "Congratulations! You ran a command.\n"
     .greeting  "\nWelcome to \033[32mNocturne Shell\033[0m!\nCopyright (c) 2026 iiPython\n\n"
     .goodbye   "\n\033[31mGoodbye!\033[0m\n"
     .no_cmd    "\033[32mNocturne\033[0m: no such command exists!\n"
     .version   "\033[32mNocturne Shell\033[0m v0.2.0, powered by \033[34m8255\033[0m.\n"
-    .help      "Commands: \033[32mhelp\033[0m, \033[32mclear\033[0m, \033[32mversion\033[0m, \033[32mexit\033[0m\n"
+    .help      "Commands: \033[32mhelp\033[0m, \033[32mmem\033[0m, \033[32mclear\033[0m, \033[32mversion\033[0m, \033[32mexit\033[0m\n"
+    .mem1      "Probing... "
+    .mem2      "\rMemory usage: "
+    .mem3      " B / 4096 B ("
+    .mem4      "%)\n"
 
     ; Commands
     .cmd_version "version"
     .cmd_help    "help"
     .cmd_exit    "exit"
     .cmd_clear   "clear"
+    .cmd_mem     "mem"
 
 terminate:
     ldi r1, 10
@@ -140,6 +144,61 @@ cmd_clear:
     swa r1, D_WRITE_STR
     ret
 
+count_mem:
+
+    ; Have we hit the stack?
+    ldi r3, 0x3000
+    cmp r2, r3
+    jeq skip_mem_count     ; If yes, then return
+
+    ; Load byte from DATA
+    lbr r3, r2
+    ldi r4, 0
+    cmp r3, r4             ; Is it NULL? (0)
+    jeq skip_byte_count    ; If it is, skip it
+    inc r1                 ; If it isn't, increment R1
+    skip_byte_count:
+
+    ; Increment DATA pointer
+    inc r2
+
+    ; And loop back
+    jmp count_mem
+
+    ; Finally, return (once we hit STACK)
+    skip_mem_count:
+    ret
+
+cmd_mem:
+    ldi r1, &mem1
+    swa r1, D_WRITE_STR
+
+    ; Starting counters
+    ldi r1, 0
+    ldi r2, 0x2000
+    cal count_mem
+    mov r2, r1             ; I love abusing r1, so move to r2
+
+    ; Logging
+    ldi r1, &mem2
+    swa r1, D_WRITE_STR
+    swa r2, D_WRITE_INT
+    ldi r1, &mem3
+    swa r1, D_WRITE_STR
+
+    ; Percentage
+    ldi r1, 100
+    mul r2, r1
+    ldi r1, 0x1000
+    div r2, r1
+
+    swa r2, D_WRITE_INT
+
+    ; Closing
+    ldi r1, &mem4
+    swa r1, D_WRITE_STR
+    ret
+
 no_such_command:
     ldi r1, &no_cmd
     swa r1, D_WRITE_STR
@@ -186,6 +245,12 @@ execute:
     jne skip_clear
     cal cmd_clear
     skip_clear:
+
+    ldi r1, &cmd_mem
+    cal check_cmd
+    jne skip_mem
+    cal cmd_mem
+    skip_mem:
 
     ; Check command status
     cal check_cmd_status
@@ -260,6 +325,9 @@ shell:
 main:
 
     ; Greeting
+    ldi r1, &clear
+    swa r1, D_WRITE_STR
+
     ldi r1, &greeting
     swa r1, D_WRITE_STR
 
